c735b29c475072e8718b21982b71d66f8e44c31d
Basanta <basantaparajuli.11@gmail.com>
2025-05-05 07:55:43 +0545

    gg

--- week-1/index.js
+++ week-1/index.js
@@ -109,3 +109,14 @@
 // const base58 = base58String; // Use the previously encoded Base58 string
 // const byteArrayFromBase58 = base58ToUint8Array(base58);
 // console.log(byteArrayFromBase58); // Output: Uint8Array(5) [72, 101, 108, 108, 111]
+
+// const bs58 = require("bs58");
+
+// function uint8ArrayToBase58(uint8Array) {
+//   return bs58.encode(uint8Array);
+// }
+
+// // Example usage:
+// const byteArray = new Uint8Array([72, 101, 108, 108, 111]); // Corresponds to "Hello"
+// const base58String = uint8ArrayToBase58(byteArray);
+// console.log(base58String); // Output: Base58 encoded string
