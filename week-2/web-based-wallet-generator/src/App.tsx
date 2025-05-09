8de83b16ca33fbdf825e6ac7dbf5d5aeaf7a4a9d
Basanta <basantaparajuli.11@gmail.com>
2025-05-09 15:21:24 +0545

    yes generation continued

--- week-2/web-based-wallet-generator/src/App.tsx
+++ week-2/web-based-wallet-generator/src/App.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import { useState } from "react";
+import { generateMnemonic } from "bip39";
 
 const App = () => {
-  return <div></div>;
+  const [mnemonic, setMnemonic] = useState("");
+
+  return (
+    <div className="main">
+      <div className="mnemonic_holder">{mnemonic}</div>
+
+      <button
+        className="genMnemonic"
+        onClick={async function () {
+          const mn = await generateMnemonic();
+          setMnemonic(mn);
+        }}
+      >
+        Create Seed Phrase
+      </button>
+    </div>
+  );
 };
 
 export default App;
